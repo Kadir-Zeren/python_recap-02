60b43e714ea88348eaf1ba71f39d545fcbd85f15
kadir-zeren <k4dzeren@gmail.com>
2024-11-01 19:57:55 +0300

    first commit

---
+++ python_02.py
@@ -0,0 +1,8 @@
+print( "kullanici ismi")
+
+# buradaki fonksiyon kare alma fonksiyonu
+print('kullanici ismi") # Burada kullanici ismi yazddiriliyor.')
+x = 5 # burada x degeri atandi
+# bu bir comment
+# bu ikinci comment
+# bu ucuncu yorum
